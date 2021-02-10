2880982feab8e6d0468ca7ec285ecac655dfadb5
khurdhed <75950115+khurdhed@users.noreply.github.com>
2021-02-11 03:35:00 +0600

    Add files via upload

--- degital seo/js/main.js
+++ degital seo/js/main.js
@@ -1,46 +1,65 @@
 // preloader
 var preloader = document.getElementById('preload')
-function myFunction(){
-    preloader.style.display='none'
+
+function myFunction() {
+    preloader.style.display = 'none'
 }
 
 // top bottom
 
-
-$(document).ready( function() {
+$(document).ready(function () {
     $('#backTop').backTop({
-        'position' : 1100,
-        'speed' : 500,
-        'color' : 'red',
+        'position': 1100,
+        'speed': 500,
+        'color': 'red',
     });
 });
 
+// quate pop up form
+
+function openForma() {
+    document.getElementById("myForma").style.display = "block";
+}
+
+function closeForma() {
+    document.getElementById("myForma").style.display = "none";
+}
+
+// chat massege
+
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+}
 
 // mean-menu
 
-$(document).ready(function(){
+$(document).ready(function () {
 
     $('#mobile-menu').meanmenu({
         meanMenuContainer: '.mobile-menu',
         meanScreenWidth: "800",
 
-});
+    });
 
 });
 
 
 
 // owl-carousel\\
- $('.owl-carousel').owlCarousel({
+$('.owl-carousel').owlCarousel({
     margin: 10,
-    loop:true,
-    nav:true,
-    center:true,
-    mouseDrag:true,
-    dots:false,
+    loop: true,
+    nav: true,
+    center: true,
+    mouseDrag: true,
+    dots: false,
     // autoplay:true,
-    autoplaytimeout:4000,
-    navText: ['←','→'],
+    autoplaytimeout: 4000,
+    navText: ['←', '→'],
     responsive: {
         0: {
             items: 1
@@ -54,27 +73,48 @@ $(document).ready(function(){
     }
 });
 
-   // wow.js\\
+// wow.js\\
 
 
-   wow = new WOW({
+wow = new WOW({
     boxClass: 'wow', // default
     animateClass: 'animated', // default
     offset: 0, // default
     mobile: true, // default
     live: true // default
-  })
-  wow.init();
+})
+wow.init();
 
 
-//   image popup
 
-// chat
+//   image popup iamg-1
 
-function openForm() {
-    document.getElementById("myForm").style.display = "block";
-  }
-  
-  function closeForm() {
-    document.getElementById("myForm").style.display = "none";
-  }
+function openImage() {
+    document.getElementById("myImage").style.display = "block";
+}
+
+function closeImage() {
+    document.getElementById("myImage").style.display = "none";
+}
+
+
+//   image popup iamg-2
+
+function openImage1() {
+    document.getElementById("myImage1").style.display = "block";
+}
+
+function closeImage1() {
+    document.getElementById("myImage1").style.display = "none";
+}
+
+
+//   image popup iamg-3
+
+function openImage2() {
+    document.getElementById("myImage2").style.display = "block";
+}
+
+function closeImage2() {
+    document.getElementById("myImage2").style.display = "none";
+}
